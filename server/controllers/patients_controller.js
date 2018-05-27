aa7ed06efb2fd81d8d787602a6e0ba12d33d1982
gongxia.chen <chengongxia1990@gmail.com>
2018-05-26 21:03:06 -0400

    Add eslint file to catch code issues

--- server/controllers/patients_controller.js
+++ server/controllers/patients_controller.js
@@ -1,40 +1,35 @@
-const Patient = require('../models/patient');
+const Patient = require("../models/patient");
 
 module.exports = {
-    // next help tell the actual error
-    create(req, res, next) {
-        console.log(req.body);
-        const patientProps = req.body;
-        Patient.create(patientProps)
-            .then(patient => res.send(patient))
-            .catch(next);
-    },
+  // next help tell the actual error
+  create(req, res, next) {
+    console.log(req.body);
+    const patientProps = req.body;
+    Patient.create(patientProps)
+      .then(patient => res.send(patient))
+      .catch(next);
+  },
 
-    edit(req, res, next) {
-        // get the id subject from the url call
-        console.log(req.body);
-        const patientProps = req.body;
+  edit(req, res, next) {
+    // get the id subject from the url call
+    console.log(req.body);
+    const patientProps = req.body;
 
-        const b = patientProps.bangou;
-        const t = patientProps.time;
+    const b = patientProps.bangou;
+    const t = patientProps.time;
 
-        // patient is the stuff returned from find
-        Patient.findOneAndUpdate(
-            { bangou: b },
-            { $push: { time: t } })
-            .then(() => Patient.findOne({ bangou: b }))
-            // get resp from the previous return
-            .then(resp => {
-                if (resp == null) {
-                    reply = JSON.stringify("ID not found!");
-                }
-                else
-                {
-                    reply = resp;
-                }
-                res.send(reply)
-            })
-            .catch(next);
-
-    }
+    // patient is the stuff returned from find
+    Patient.findOneAndUpdate({ bangou: b }, { $push: { time: t } })
+      .then(() => Patient.findOne({ bangou: b }))
+      // get resp from the previous return
+      .then(resp => {
+        if (resp == null) {
+          reply = JSON.stringify("ID not found!");
+        } else {
+          reply = resp;
+        }
+        res.send(reply);
+      })
+      .catch(next);
+  }
 };
